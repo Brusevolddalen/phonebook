9f7165bafbf1b99e842d9cfcf957fa12015fbfa5
Brusevolddalen <martin_brusevolddalen@hotmail.com>
2017-09-18 20:48:05 +0200

    Added modal loader

--- index.php
+++ index.php
@@ -87,6 +87,7 @@
   </div>
 
   <script src="js/app.js"></script>
+  <script src="js/loadModal.js"></script>
   <script src="js/controllers/MainController.js"></script>
 
 
---
+++ js/loadModal.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.category-modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .8, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+      height: "10%",
+    }
+  );
+});
