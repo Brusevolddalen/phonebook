d5a219517d08624502a2458ec18eed0f0a6fd106
Brusevolddalen <martin_brusevolddalen@hotmail.com>
2017-09-19 20:00:24 +0200

    Testing automatic refresh after deleting contact

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,17 +1,17 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
-  $http.get('php/loadContacts.php')
-    .then(function(response) {
+  $scope.getContacts = function() {
+    $http.get('php/loadContacts.php')
+      .then(function(response) {
 
-      var data = response.data;
-      var status = response.status;
-      var statusText = response.statusText;
-      var headers = response.headers;
-      var config = response.config;
-
-      $scope.contacts = data;
-    });
-
+        var data = response.data;
+        var status = response.status;
+        var statusText = response.statusText;
+        var headers = response.headers;
+        var config = response.config;
+        $scope.contacts = data;
+      });
+  };
   $scope.deleteContact = function(phone) {
 
     $(document).on('click', '.delete-btn', function(event) {
@@ -21,17 +21,18 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
       let phone = $(tds[2]).text();
 
       $http({
-        url: 'php/deleteContact.php',
-        method: "POST",
-        data: 'phoneNumber=' + phone,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      .then(function(response) {
-        console.log(response.data);
-      });
-      
+          url: 'php/deleteContact.php',
+          method: "POST",
+          data: 'phoneNumber=' + phone,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then(function(response) {
+          console.log(response.data);
+        });
+
     });
   };
+  $scope.getContacts();
 }]);
